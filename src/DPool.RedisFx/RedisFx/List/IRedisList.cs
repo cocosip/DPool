9b83db9769f78bbae5e03ef8ba16ff30e5fcc8e0
virtual <1185513330@qq.com>
2020-04-20 22:49:10 +0800

    commit Signed-off-by: virtual <1185513330@qq.com>

---
+++ src/DPool.RedisFx/RedisFx/List/IRedisList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DPool.RedisFx.List
+{
+
+    /// <summary>Redis链表
+    /// </summary>
+    public interface IRedisList
+    {
+
+    }
+
+    /// <summary>Redis链表
+    /// </summary>
+    public interface IRedisList<T> : IRedisList
+    {
+
+    }
+}
---
+++ src/DPool.RedisFx/RedisFx/List/RedisList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DPool.RedisFx.List
+{
+    /// <summary>Redis链表
+    /// </summary>
+    public class RedisList<T> : IRedisList<T>
+    {
+
+    }
+}
